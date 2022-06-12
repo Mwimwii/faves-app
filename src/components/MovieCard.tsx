63588389f82e7b5ff8d470529b010c3596e34736
gooby <mpnyirongo@gmail.com>
2022-06-12 07:36:12 +0200

    Added simple loader for movie cards

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -11,24 +11,27 @@ const Spinner: React.FC<{ size: number; color: string; colorSize: number }> = ({
 }
 const MovieCard: React.FC<{ movie: Movie }> = ({ movie }) => {
     const [isLoading, setIsLoading] = React.useState(false);
+    const [isLoadingImage, setIsLoadingImage] = React.useState(true);
     const { push } = useStore()
-    
     const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         event.preventDefault();
         setIsLoading(true);
         setTimeout(() => {
             setIsLoading(false);
         }, 300);
-        console.log('You have liked',movie.title);
+        console.log('You have liked', movie.title);
         push(movie)
     }
     return (
         <div className=' shadow-lg mt-4 mx-3 rounded-md bg-gray-100'>
-            <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title} className='w-1/4 sm:w-full rounded-tl-lg sm:rounded-t-lg' />
-            <div className="w-1/4 sm:w-full flex justify-evenly rounded-bl-md sm:rounded-b-md bg-neutral-300">
-                {!isLoading ? <button className="p-2 font-light" onClick={handleClick}>Like</button>
-                    : <p className="p-2 font-light">Loading...</p>}
-                </div>
+
+            {isLoadingImage && <div style={{ maxHeight: '200px', height: '200px' }} className="w-1/4 sm:w-full rounded-tl-lg sm:rounded-t-lg bg-slate-500 animate-pulse"></div>}
+            <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title} className={`w-1/4 sm:w-full rounded-tl-lg sm:rounded-t-lg ${isLoadingImage && 'hidden'}`} onLoad={() => setIsLoadingImage(false)} />
+
+            <div className="w-1/4 sm:w-full rounded-bl-md sm:rounded-b-md bg-neutral-300">
+                {!isLoading ? <button className="p-2 w-full text-center font-light" onClick={handleClick}>Like</button>
+                    : <button className="rounded-bl-md sm:rounded-b-md p-2 w-full text-center font-light bg-neutral-400 cursor-default">Loading...</button>}
+            </div>
         </div>
     )
 }

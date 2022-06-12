c7176322e21c17bc197f5c9552c8ecb4a6da8bf6
gooby <mpnyirongo@gmail.com>
2022-06-12 06:29:49 +0200

    Applied the store to the movie card

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Movie } from "../models";
+import React from 'react';
+import { Movie } from '../models';
+import { useStore } from '../store'
 
 const Spinner: React.FC<{ size: number; color: string; colorSize: number }> = ({ size, color, colorSize }) => {
     return (
@@ -10,6 +11,8 @@ const Spinner: React.FC<{ size: number; color: string; colorSize: number }> = ({
 }
 const MovieCard: React.FC<{ movie: Movie }> = ({ movie }) => {
     const [isLoading, setIsLoading] = React.useState(false);
+    const { push } = useStore()
+    
     const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         event.preventDefault();
         setIsLoading(true);
@@ -17,11 +20,12 @@ const MovieCard: React.FC<{ movie: Movie }> = ({ movie }) => {
             setIsLoading(false);
         }, 300);
         console.log('You have liked',movie.title);
+        push(movie)
     }
     return (
-        <div className='px-2 pb-4 shadow-lg mt-4 mx-3 rounded-md bg-gray-100'>
-            <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title} className='rounded-lg pt-2' />
-            <div className="flex justify-evenly rounded-md bg-neutral-300 mt-2 space-x-4">
+        <div className=' shadow-lg mt-4 mx-3 rounded-md bg-gray-100'>
+            <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title} className='w-1/4 sm:w-full rounded-tl-lg sm:rounded-t-lg' />
+            <div className="w-1/4 sm:w-full flex justify-evenly rounded-bl-md sm:rounded-b-md bg-neutral-300">
                 {!isLoading ? <button className="p-2 font-light" onClick={handleClick}>Like</button>
                     : <p className="p-2 font-light">Loading...</p>}
                 </div>

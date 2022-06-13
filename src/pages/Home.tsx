e2aa9ca7ae122420246694cf4c92cb4a7d6f776b
gooby <mpnyirongo@gmail.com>
2022-06-13 13:38:48 +0200

    Finalized the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,33 @@
 import React from 'react'
-import { MovieDBResponse, Movie } from '../models'
-import { MovieCard } from '../components'
-
-const useMovies = () => {
-    const [movies, setMovies] = React.useState<Movie[]>([])
-    React.useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=d0f5f2e135336200362af8a1a73acb17&page=${1}`)
-            .then(res => res.json())
-            .then((data: MovieDBResponse) => setMovies(data.results))
-    }, [])
-    return { movies }
-}
-
-
+import { HeaderSelection, MovieCard } from '../components'
+import { useMovies } from '../hooks'
 const Home: React.FC = () => {
-    const { movies } = useMovies()
+    const [page, setPage] = React.useState(1)
+    const { movies, likedMovies } = useMovies(page)
     return (
-        <>
-            <div className="flex flex-wrap">
-                {movies && movies.map(movie => (
-                    <div className="w-full sm:w-1/4 p-2 sm:p-2">
-                        <MovieCard key={movie.id} movie={movie} />
+        <div className='w-full h-screen overflow-scroll pt-16 z-10 no-scrollbar pb-20 md:pb-0 flex flex-col items-center'>
+            <HeaderSelection title='Top Movies' />
+            <div className='w-5/6 flex flex-wrap justify-center'>
+                {movies && movies.map((movie, movieIndex) => (
+                    <div className='card relative w-full sm:w-1/5 p-2'>
+                        {
+                            likedMovies.find(likedMovie => likedMovie.id === movie.id) ?
+                            <>
+                                <div className='pos__item z-30 top-3 left-4 w-5 h-5 text-blue-200 rounded-full absolute text-xs text-center items-center justify-center bg-red-500 hidden sm:flex'>{movieIndex + 1}</div>
+                                <MovieCard key={`${movie.id}${movieIndex}`} movie={movie} active={true} /> 
+                            </>
+                                :
+                            <>
+                                <div className='pos__item z-30 top-3 left-4 w-5 h-5 text-blue-200 rounded-full absolute text-xs text-center items-center justify-center bg-slate-600 hidden sm:flex'>{movieIndex + 1}</div>
+                                <MovieCard key={`${movie.id}${movieIndex}`} movie={movie} active={false} />
+                                </>
+                        }
                     </div>
                 ))}
+
+                {movies.length !== 0 && <button className='w-full p-2 bg-slate-600 text-slate-100 font-bold mb-1 sm:mb-4 rounded' onClick={() => { setPage(page + 1) }}> Load More </button>}
             </div>
-        </>
+        </div>
     )
 }
 

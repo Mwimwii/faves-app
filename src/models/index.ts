5f4f7a1870b3af3d686cc70feb05283e3d9f99e1
gooby <mpnyirongo@gmail.com>
2022-06-13 13:31:17 +0200

    Migrate movie state interface

--- src/models/index.ts
+++ src/models/index.ts
@@ -16,8 +16,17 @@ export interface Movie {
     overview: string
     popularity: number
     poster_path: string
-    release_date: Date
+    release_date: string
     video: boolean
     vote_average: number
     vote_count: number
+}
+
+export interface MovieState {
+    movies: Movie[],
+    likedMovies: Movie[]
+    load: (movie: Movie[]) => void
+    push: (movie: Movie) => void
+    pop: (movie: Movie) => void
+    flush: () => void
 }
